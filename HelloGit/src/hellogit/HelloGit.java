80451a8476bbd1fbf3fc90998caec7cf8d3939b8
Alissa <Alissa@LAPTOP-CNKRD13V>
2017-01-24 21:33:48 -0500

    Final Version test 2.0

---
+++ HelloGit/src/hellogit/HelloGit.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hellogit;
+
+/**
+ *
+ * @author Alissa
+ */
+public class HelloGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello Git! My name is WolfgangKals :o) ");
+        System.out.println("Hello Jared, pineapple on pizza is not good");
+    }
+    
+}
